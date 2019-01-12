a408491ce1b562e90c684f936b32c706bc61f66f
malu1798 <malu1798@student.ju.se>
2019-01-12 10:57:25 +0100

    added code for append and read from file etc.

--- GUI_with_graph.py
+++ GUI_with_graph.py
@@ -3,7 +3,8 @@
 # default
 import sys
 #import numpy as np
-import time
+#import time
+from time import sleep, strftime, time
 
 # matplotlib
 #import matplotlib.pyplot as plt
@@ -28,6 +29,16 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtGui import QFont
 
+import random
+
+
+# for file and temp
+from gpiozero import CPUTemperature
+
+
+#global var
+cpu = CPUTemperature()
+
 
 class Window(QDialog):
     def __init__(self, parent=None):
@@ -35,24 +46,36 @@ class Window(QDialog):
 
         # a figure instance to plot on
         self.figure = plt.figure()
+        self.figure2 = plt.figure()
 
         # this is the Canvas Widget that displays the `figure`
         # it takes the `figure` instance as a parameter to __init__
         self.canvas = FigureCanvas(self.figure)
 
+        # this is the Canvas Widget that displays the `figure`
+        # it takes the `figure` instance as a parameter to __init__
+        self.canvas2 = FigureCanvas(self.figure2)
+
         # this is the Navigation widget
         # it takes the Canvas widget and a parent
         self.toolbar = NavigationToolbar(self.canvas, self)
+        self.toolbar2 = NavigationToolbar(self.canvas2, self)
 
         # Just some button connected to `plot` method
         self.button = QPushButton('Plot')
         self.button.clicked.connect(self.plot)
+
+        self.button2 = QPushButton('Plot2')
+        self.button2.clicked.connect(self.plotLive)
 
         # set the layout
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
         layout.addWidget(self.button)
+        layout.addWidget(self.toolbar2)
+        layout.addWidget(self.canvas2)
+        layout.addWidget(self.button2)
         self.setLayout(layout)
 
     def plot(self):
@@ -74,6 +97,56 @@ class Window(QDialog):
 
         # refresh canvas
         self.canvas.draw()
+
+    def plotLive(self):
+
+        # instead of ax.hold(False)
+        self.figure2.clear()
+
+        # create an axis
+        # self refers to a figure
+        ax1 = self.figure2.add_subplot(1, 1, 1)
+
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
+
+        ax1.clear()
+        # plt.clf()
+
+        # plot data
+        append_to_file()
+        xs, ys = read_from_file()
+
+        ax1.scatter(xs, ys)
+        ax1.plot(xs, ys)
+
+        # refresh canvas
+        self.canvas2.draw()
+
+
+def read_from_file():
+    # open file and append it to array
+    graph_data = open('/home/pi/projectScreen/cpu_temp.csv', 'r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(float(y))
+
+    return xs, ys
+
+
+def append_to_file():
+    temp = cpu.temperature
+    with open("/home/pi/projectScreen/cpu_temp.csv", "a") as log:
+        log.write("{},{}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(temp)))
+
+
+def get_cpu_temp():
+    return cpu.temperature
 
 
 if __name__ == '__main__':
